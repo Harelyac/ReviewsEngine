714f45b126183344f1a3ce58453ff991c0445756
harelyac <harelyac@gmail.com>
2020-05-18 18:04:00 +0300

    fix lex

--- src/webdata/Main.java
+++ src/webdata/Main.java
@@ -13,7 +13,7 @@ public class Main {
         siw.slowWrite("1000.txt", "src//webdata");
         IndexReader ir = new IndexReader("src//webdata");
 
-        System.out.println(ir.getTokenCollectionFrequency("to"));
+        System.out.println(ir.getReviewLength(1));
 
         //test();
     }
